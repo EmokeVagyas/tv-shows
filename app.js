564af17b1bfaf295bf723287eba2028bc764f4d3
Emoke <emokevagyas@gmail.com>
2024-10-06 01:25:24 +0300

    Created demo page to practice API requests

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+})
+
+const makeImages = (shows) => {
+    document.getElementById('results').innerHTML = '';
+
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.getElementById('results').append(img)
+        }
+    }
+}
